7861a33233738c88809f5daf666aa4fce51bf6b1
vision1v1 <vision1v1@163.com>
2024-01-02 13:11:15 +0800

    调试代码

--- debug/debug_deepspeed.py
+++ debug/debug_deepspeed.py
@@ -1,10 +1,28 @@
 import deepspeed
 import torch
+from torch import nn
+import dataclasses
 
 def test_init_distributed():
     """"""
     deepspeed.init_distributed(dist_backend='nccl', rank=0, world_size=2)
 
+class Args:
+    local_rank = 0
+    deepscale_config = {
+        "train_batch_size": 8,
+        "gradient_accumulation_steps": 1,
+        "optimizer": {
+            "type": "Adam",
+            "params": {
+            "lr": 0.00015
+            }
+        },
+        "fp16": {
+            "enabled": True
+        },
+        "zero_optimization": True
+    }
 
 def test_initialize():
     """
@@ -12,9 +30,11 @@ def test_initialize():
     参考
     https://www.deepspeed.ai/getting-started/
     """
-    model_engine, optimizer, training_dataloader, lr_scheduler = deepspeed.initialize(args=..., 
-                                                                                      model=...,
-                                                                                      model_parameters=...)
+    args = Args()
+    model = nn.Linear(in_features=6, out_features=5)
+    model_engine, optimizer, training_dataloader, lr_scheduler = deepspeed.initialize(args=args, 
+                                                                                      model=model,
+                                                                                      model_parameters=model.parameters())
     
     print("training...")
 
--- debug/debug_mpi.py
+++ debug/debug_mpi.py
@@ -1,1 +1,4 @@
 from mpi4py import MPI # block ?
+
+# 参考
+# https://mpi4py.readthedocs.io/en/stable/tutorial.html
